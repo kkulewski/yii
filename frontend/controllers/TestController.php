67fc21b84a1cb245dc297e0cd65780038bf286ed
Krzysztof Kulewski <krzysztofkulewski@gmail.com>
2017-05-14 01:43:31 +0200

    Add single random test mode

--- frontend/controllers/TestController.php
+++ frontend/controllers/TestController.php
@@ -18,10 +18,11 @@ class TestController extends \yii\web\Controller
 		{
             $session = Yii::$app->session;
             $session->open();
-            $session->set('id',$id);
-            $session->set('answers_correct',array());
-            $session->set('answers_wrong',array());
-			$session->set('currentQuestionNumber',1);
+            $session->set('id', $id);
+            $session->set('answers_correct', array());
+            $session->set('answers_wrong', array());
+			$session->set('answered', array());
+			$session->set('currentQuestionNumber', 1);
 			
 			
 			$show = Yii::$app->getRequest()->getQueryParam('show');
@@ -70,6 +71,7 @@ class TestController extends \yii\web\Controller
         $id = $session->get('id');
         $answers_correct  = $session->get('answers_correct');
         $answers_wrong = $session->get('answers_wrong');
+		$answered = $session->get('answered');
         $zestaw = Zestaw::findOne($id);
 		
 		
@@ -83,11 +85,7 @@ class TestController extends \yii\web\Controller
 		{
             // zapisz wynik odpowiedzi
             if($receivedAnswer->isAnswerCorrect($zestaw->wordsDictionary(), $reverse) == TRUE)
-			{
-				// update question number counter
-				$questionNumber = $session->get('currentQuestionNumber');
-				$session->set('currentQuestionNumber', $questionNumber + 1);
-				
+			{	
                 $answers_correct[] = $receivedAnswer->pairNumber;
                 $session->set('answers_correct', $answers_correct);
 				$this->Correct = count ($answers_correct);
@@ -98,10 +96,19 @@ class TestController extends \yii\web\Controller
                 $session->set('answers_wrong', $answers_wrong);
 				$this->Wrong = count ($answers_wrong);
             }
+			
+			$answered[] = $receivedAnswer->pairNumber;
+			$session->set('answered', $answered);
+			
+			
+			// update question number counter
+			$questionNumber = $session->get('currentQuestionNumber');
+			$session->set('currentQuestionNumber', $questionNumber + 1);
         }
 
         // prepare data for test
         $testData = [
+			'answered' => $answered,
             'answers_correct' => $answers_correct,
             'answers_wrong' => $answers_wrong,
             'wordsDictionary' => $zestaw->wordsDictionary(),
@@ -118,9 +125,13 @@ class TestController extends \yii\web\Controller
 		{
 			$testMode = new SequentialTestMode($testData);
 		}
+		else if ( $mode == 3)
+		{
+			$testMode = new SingleRandomTestMode($testData);
+		}
 		else
 		{
-			$testMode = new RandomTestMode($testData);
+			$testMode = new SingleRandomTestMode($testData);
 		}
 		
 		// handle next pair
@@ -169,6 +180,42 @@ class RandomTestMode extends TestMode
         foreach($this->answers_correct as $correctGuess)
 		{
             unset($this->wordsDictionary[$correctGuess]);
+        }
+
+		// if no more pairs left - finish test
+        if (count($this->wordsDictionary) == 0) 
+			return null;
+		
+		if($reverse == TRUE)
+		{
+			$q = 1;
+			$a = 0;
+		}
+		else
+		{
+			$q = 0;
+			$a = 1;
+		}
+
+		// get random pair
+        $pairNumber = array_rand($this->wordsDictionary);
+        $nextWord = $this->wordsDictionary[$pairNumber];
+        $pair['pairNumber'] = $pairNumber;
+        $pair['pairQuestion'] = $nextWord[$q];
+        $pair['pairAnswer'] = $nextWord[$a];
+        
+        return $pair;
+    }
+}
+
+class SingleRandomTestMode extends TestMode 
+{
+    public function getPair($reverse) 
+	{
+		// remove correctly guessed pairs from set
+        foreach($this->answered as $answeredQuestion)
+		{
+            unset($this->wordsDictionary[$answeredQuestion]);
         }
 
 		// if no more pairs left - finish test
