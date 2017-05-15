7fd16aa2c405cb31b4b2194675802cab92482d10
Krzysztof Kulewski <krzysztofkulewski@gmail.com>
2017-05-15 20:31:16 +0200

    Reorganize test view

--- frontend/views/test/view.php
+++ frontend/views/test/view.php
@@ -20,22 +20,38 @@ $this->params['breadcrumbs'][] = $this->title;
 </div>	
 <p></p>
 <div class="col-lg-12">
-	<h2>Tryb nauki</h2>
+	<h2><br>Tryb sprawdzenia wiedzy<br></h2><br>
 	<div class="col-lg-6">
-		<?= Html::a('Losowo 1 >> 2 do skutku', [ 'test/next', 'mode' => 1, 'reverse' => FALSE, 'singleMode' => FALSE, ], [ 'class' => 'btn btn-primary' ]) ?><br/><br/>
-		<?= Html::a('Losowo 2 >> 1 do skutku', [ 'test/next', 'mode' => 1, 'reverse' => TRUE, 'singleMode' => FALSE, ], [ 'class' => 'btn btn-primary' ]) ?><br/><br/>
-		<?= Html::a('Losowo 1 >> 2 pojedynczo', [ 'test/next', 'mode' => 1, 'reverse' => FALSE, 'singleMode' => TRUE, ], [ 'class' => 'btn btn-primary' ]) ?><br/><br/>
-		<?= Html::a('Losowo 2 >> 1 pojedynczo', [ 'test/next', 'mode' => 1, 'reverse' => TRUE, 'singleMode' => TRUE ], [ 'class' => 'btn btn-primary' ]) ?>
-	</div>
-	<div class="col-lg-6">
-		<?= Html::a('Sekwencyjnie 1 >> 2 do skutku', [ 'test/next', 'mode' => 2, 'reverse' => FALSE, 'singleMode' => FALSE, ], [ 'class' => 'btn btn-primary' ]) ?><br/><br/> 
-		<?= Html::a('Sekwencyjnie 2 >> 1 do skutku', [ 'test/next', 'mode' => 2, 'reverse' => TRUE, 'singleMode' => FALSE, ], [ 'class' => 'btn btn-primary' ]) ?><br/><br/>
-		<?= Html::a('Sekwencyjnie 1 >> 2 pojedynczo', [ 'test/next', 'mode' => 2, 'reverse' => FALSE, 'singleMode' => TRUE,], [ 'class' => 'btn btn-primary' ]) ?><br/><br/> 
-		<?= Html::a('Sekwencyjnie 2 >> 1 pojedynczo', [ 'test/next', 'mode' => 2, 'reverse' => TRUE, 'singleMode' => TRUE,], [ 'class' => 'btn btn-primary' ]) ?>
+		<?= Html::a(''.$zestaw->jezyk1->nazwa.' >> '.$zestaw->jezyk2->nazwa.'', [ 'test/next', 'mode' => 1, 'reverse' => FALSE, 'singleMode' => TRUE, 'examMode' => TRUE, ], [ 'class' => 'btn btn-primary' ]) ?><br><br>
+		<?= Html::a(''.$zestaw->jezyk2->nazwa.' >> '.$zestaw->jezyk1->nazwa.'', [ 'test/next', 'mode' => 1, 'reverse' => TRUE, 'singleMode' => TRUE, 'examMode' => TRUE, ], [ 'class' => 'btn btn-primary' ]) ?>
 	</div>
 </div>
 <div class="col-lg-12">
-	<h2>Tryb sprawdzenia wiedzy</h2>
-<?= Html::a('Sprawdzian 1 >> 2', [ 'test/next', 'mode' => 1, 'reverse' => FALSE, 'singleMode' => TRUE, 'examMode' => TRUE, ], [ 'class' => 'btn btn-primary' ]) ?><br/><br/> 
-<?= Html::a('Sprawdzian 2 >> 1', [ 'test/next', 'mode' => 1, 'reverse' => TRUE, 'singleMode' => TRUE, 'examMode' => TRUE, ], [ 'class' => 'btn btn-primary' ]) ?>
+	<h2><br/>Tryb nauki</h2>
+	<div class="col-lg-6">
+		<div class="col-lg-4">
+			<h3>Losowo - do skutku</h3>
+			<?= Html::a(''.$zestaw->jezyk1->nazwa.' >> '.$zestaw->jezyk2->nazwa.'', [ 'test/next', 'mode' => 1, 'reverse' => FALSE, 'singleMode' => FALSE, ], [ 'class' => 'btn btn-primary' ]) ?><br/><br/>
+			<?= Html::a(''.$zestaw->jezyk2->nazwa.' >> '.$zestaw->jezyk1->nazwa.'', [ 'test/next', 'mode' => 1, 'reverse' => TRUE, 'singleMode' => FALSE, ], [ 'class' => 'btn btn-primary' ]) ?><br/><br/>
+		</div>
+	
+		<div class="col-lg-4">
+			<h3>Losowo - pojedynczo</h3>
+			<?= Html::a(''.$zestaw->jezyk1->nazwa.' >> '.$zestaw->jezyk2->nazwa.'', [ 'test/next', 'mode' => 1, 'reverse' => FALSE, 'singleMode' => TRUE, ], [ 'class' => 'btn btn-primary' ]) ?><br/><br/>
+			<?= Html::a(''.$zestaw->jezyk2->nazwa.' >> '.$zestaw->jezyk1->nazwa.'', [ 'test/next', 'mode' => 1, 'reverse' => TRUE, 'singleMode' => TRUE ], [ 'class' => 'btn btn-primary' ]) ?>
+		</div>
+	</div>
+	<div class="col-lg-6">
+		<div class="col-lg-4">
+			<h3>Sekwencyjnie - do skutku</h3>
+			<?= Html::a(''.$zestaw->jezyk1->nazwa.' >> '.$zestaw->jezyk2->nazwa.'', [ 'test/next', 'mode' => 2, 'reverse' => FALSE, 'singleMode' => FALSE, ], [ 'class' => 'btn btn-primary' ]) ?><br/><br/> 
+			<?= Html::a(''.$zestaw->jezyk2->nazwa.' >> '.$zestaw->jezyk1->nazwa.'', [ 'test/next', 'mode' => 2, 'reverse' => TRUE, 'singleMode' => FALSE, ], [ 'class' => 'btn btn-primary' ]) ?><br/><br/>
+		</div>
+		<div class="col-lg-4">
+			<h3>Sekwencyjnie - pojedynczo</h3>
+			<?= Html::a(''.$zestaw->jezyk1->nazwa.' >> '.$zestaw->jezyk2->nazwa.'', [ 'test/next', 'mode' => 2, 'reverse' => FALSE, 'singleMode' => TRUE,], [ 'class' => 'btn btn-primary' ]) ?><br/><br/> 
+			<?= Html::a(''.$zestaw->jezyk2->nazwa.' >> '.$zestaw->jezyk1->nazwa.'', [ 'test/next', 'mode' => 2, 'reverse' => TRUE, 'singleMode' => TRUE,], [ 'class' => 'btn btn-primary' ]) ?>
+		</div>
+	</div>
 </div>
+
